ba8676755e2e6db61a29797c3ed859918df676a9
3ddelano <3ddelano@gmail.com>
2023-12-13 18:33:40 +0530

    Rename result to result_code to match convention

--- src/mods_interface.cpp
+++ src/mods_interface.cpp
@@ -14,7 +14,7 @@ Dictionary IEOS::mods_interface_copy_mod_info(Ref<RefCounted> p_options) {
     EOS_EResult res = EOS_Mods_CopyModInfo(s_modsInterface, &options, &outEnumeratedMods);
 
     Dictionary ret;
-    ret["result"] = static_cast<int>(res);
+    ret["result_code"] = static_cast<int>(res);
     ret["mods"] = eosg_mods_mod_info_to_dict_and_release(outEnumeratedMods);
     return ret;
 }
